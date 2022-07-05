fb3b7b9050b4dff6755efe05f6d7bcb0b4d40a0e
이유나 <yuna0560@naver.com>
2022-07-05 21:27:17 +0900

    add Email, User model

---
+++ src/main/java/com/hot/place/model/user/User.java
@@ -0,0 +1,195 @@
+package com.hot.place.model.user;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.time.LocalDateTime.now;
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+
+public class User {
+
+    private final Long seq;
+
+    private final Email email;
+
+    private String name;
+
+    private String password;
+
+    private String profileImageUrl;
+
+    private int loginCount;
+
+    private LocalDateTime lastLoginAt;
+
+    private final LocalDateTime createAt;
+
+    public User(Email email, String name, String password) {
+        this(email, name, password, null);
+    }
+
+    public User(Email email, String name, String password, String profileImageUrl) {
+        this(null, email, name, password, profileImageUrl, 0, null, null);
+    }
+
+    public User(Long seq, Email email, String name, String password, String profileImageUrl, int loginCount, LocalDateTime lastLoginAt, LocalDateTime createAt) {
+        checkArgument(email != null, "email must be provided.");
+        checkArgument(isNotEmpty(name), "name must be provided.");
+        checkArgument(
+                name.length() >= 1 && name.length() <= 10,
+                "name length must be between 1 and 10 characters."
+        );
+        checkArgument(password != null, "password must be provided.");
+        checkArgument(
+                profileImageUrl == null || profileImageUrl.length() <= 255,
+                "profileImageUrl length must be less than 255 characters."
+        );
+
+        this.seq = seq;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
+        this.loginCount = loginCount;
+        this.lastLoginAt = lastLoginAt;
+        this.createAt = defaultIfNull(createAt, now());
+    }
+
+    public void afterLoginSuccess() {
+        loginCount++;
+        lastLoginAt = now();
+    }
+
+    public Long getSeq() {
+        return seq;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public int getLoginCount() {
+        return loginCount;
+    }
+
+    public LocalDateTime getLastLoginAt() {
+        return lastLoginAt;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return Objects.equals(user.seq, seq);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(seq);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("seq", seq)
+                .append("name", name)
+                .append("email", email)
+                .append("password", "[PROTECTED]")
+                .append("profileImageUrl", profileImageUrl)
+                .append("loginCount", loginCount)
+                .append("lastLoginAt", lastLoginAt)
+                .append("createAt", createAt)
+                .toString();
+    }
+
+    static public class Builder {
+        private Long seq;
+        private Email email;
+        private String name;
+        private String password;
+        private String profileImageUrl;
+        private int loginCount;
+        private LocalDateTime lastLoginAt;
+        private LocalDateTime createAt;
+
+        public Builder() {}
+
+        public Builder(User user) {
+            this.seq = user.seq;
+            this.email = user.email;
+            this.name = user.name;
+            this.password = user.password;
+            this.loginCount = user.loginCount;
+            this.lastLoginAt = user.lastLoginAt;
+            this.createAt = user.createAt;
+        }
+
+        public Builder seq(Long seq) {
+            this.seq = seq;
+            return this;
+        }
+
+        public Builder email(Email email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder profileImageUrl(String profileImageUrl) {
+            this.profileImageUrl = profileImageUrl;
+            return this;
+        }
+
+        public Builder loginCount(int loginCount) {
+            this.loginCount = loginCount;
+            return this;
+        }
+
+        public Builder lastLoginAt(LocalDateTime lastLoginAt) {
+            this.lastLoginAt = lastLoginAt;
+            return this;
+        }
+
+        public Builder createAt(LocalDateTime createAt) {
+            this.createAt = createAt;
+            return this;
+        }
+        
+        public User Build() {
+            return new User(seq, email, name, password, profileImageUrl, loginCount, lastLoginAt, createAt);
+        }
+    }
+
+}
