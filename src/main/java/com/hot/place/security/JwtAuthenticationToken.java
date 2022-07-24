9bb4ed272f2ef28efe392c4d8c4f5b66dc1d07c2
이유나 <yuna0560@naver.com>
2022-07-24 22:33:49 +0900

    [fix] 생성자 수정 [add] overriding method 구현

--- src/main/java/com/hot/place/security/JwtAuthenticationToken.java
+++ src/main/java/com/hot/place/security/JwtAuthenticationToken.java
@@ -1,5 +1,7 @@
 package com.hot.place.security;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -9,13 +11,13 @@ import java.util.Collection;
  * 인증 주체
  * 로그인 전에는 String 타입이고, 로그인 후에는 {@link JwtAuthentication} 타입이다.
  * */
-public class JwtAuthenticationToken extends AbstractAuthenticationToken { //todo
+public class JwtAuthenticationToken extends AbstractAuthenticationToken { // Authentication 객체 구현
 
     private final Object principal;
 
     private String credentials;
 
-    public JwtAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal, String credentials) {
+    public JwtAuthenticationToken(String principal, String credentials) {
         super(null);
         super.setAuthenticated(false);
 
@@ -31,28 +33,39 @@ public class JwtAuthenticationToken extends AbstractAuthenticationToken { //todo
         this.credentials = credentials;
     }
 
+    AuthenticationRequest authenticationRequest() {
+        return new AuthenticationRequest(String.valueOf(principal), credentials);
+    }
+
     @Override
     public Object getCredentials() {
-        return null;
+        return credentials;
     }
 
     @Override
     public Object getPrincipal() {
-        return null;
+        return principal;
     }
 
     @Override
-    public void setAuthenticated(boolean authenticated) {
-        super.setAuthenticated(authenticated);
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        if (isAuthenticated) {
+            throw new IllegalArgumentException("Cannot set this token to trusted - use constructor which takes a GrantedAuthority list instead");
+        }
+        super.setAuthenticated(false);
     }
 
     @Override
     public void eraseCredentials() {
         super.eraseCredentials();
+        credentials = null;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("principal", principal)
+                .append("credentials", "[PROTECTED]")
+                .toString();
     }
 }
