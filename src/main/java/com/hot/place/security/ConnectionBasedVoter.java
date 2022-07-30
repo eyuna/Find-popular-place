27cc9ab5b6fc660591ddbd7676cf1b5dbc7a41bc
이유나 <yuna0560@naver.com>
2022-07-30 22:56:38 +0900

    [add] 커스텀 voter 추가

--- src/main/java/com/hot/place/security/ConnectionBasedVoter.java
+++ src/main/java/com/hot/place/security/ConnectionBasedVoter.java
@@ -1,17 +1,17 @@
 package com.hot.place.security;
 
-import com.hot.place.service.user.UserService;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.FilterInvocation;
-import org.springframework.test.web.client.RequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 import java.util.function.Function;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static org.apache.commons.lang3.ClassUtils.isAssignable;
 
 public class ConnectionBasedVoter implements AccessDecisionVoter<FilterInvocation> {
     // Invocation 에 체크할 대상이 담겨 있다.
@@ -21,8 +21,6 @@ public class ConnectionBasedVoter implements AccessDecisionVoter<FilterInvocatio
 
     private final Function<String, Long> idExtractor;
     // 1개의 인자(Type T)를 받고 1개의 객체(Type R)를 리턴하는 함수형 인터페이스
-
-    private UserService userService;
 
     public ConnectionBasedVoter(RequestMatcher requiresAuthorizationRequestMatcher, Function<String, Long> idExtractor) {
         checkArgument(requiresAuthorizationRequestMatcher != null, "requiresAuthorizationRequestMatcher must be provided.");
@@ -36,17 +34,38 @@ public class ConnectionBasedVoter implements AccessDecisionVoter<FilterInvocatio
     public int vote(Authentication authentication, FilterInvocation fi, Collection<ConfigAttribute> attributes) {
         HttpServletRequest request = fi.getRequest();
 
+        if (!requiresAuthorization(request)) {
+            return ACCESS_GRANTED;
+        }
+
+        JwtAuthentication jwtAuth = (JwtAuthentication) authentication.getPrincipal();
+        Long targetId = obtainTargetId(request);
+
+        // 본인 자신
+        if (jwtAuth.id.equals(targetId)) {
+            return ACCESS_GRANTED;
+        }
+
         return ACCESS_DENIED;
+    }
+
+
+    private boolean requiresAuthorization(HttpServletRequest request) {
+        return requiresAuthorizationRequestMatcher.matches(request);
+    }
+
+    private Long obtainTargetId(HttpServletRequest request) {
+        return idExtractor.apply(request.getRequestURI());
     }
 
     @Override
     public boolean supports(ConfigAttribute attribute) {
-        return false;
+        return true;
     }
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return false;
+        return isAssignable(FilterInvocation.class, clazz);
     }
 
 }
