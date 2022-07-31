f6a2dc725cf8e927cb41b7cd560de6e531148cd1
이유나 <yuna0560@naver.com>
2022-07-31 23:24:16 +0900

    [add] Swagger 설정 파일

---
+++ src/main/java/com/hot/place/configure/Swagger2Configure.java
@@ -0,0 +1,82 @@
+package com.hot.place.configure;
+
+import com.hot.place.configure.support.Pageable;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static java.util.Collections.singleton;
+import static java.util.Collections.singletonList;
+import static springfox.documentation.builders.RequestHandlerSelectors.withMethodAnnotation;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Configure {
+
+    private final JwtTokenConfigure jwtTokenConfigure;
+
+    public Swagger2Configure(JwtTokenConfigure jwtTokenConfigure) {
+        this.jwtTokenConfigure = jwtTokenConfigure;
+    }
+
+    @Bean
+    public Docket restApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .ignoredParameterTypes(AuthenticationPrincipal.class, Pageable.class)
+                .securitySchemes(singletonList(apiKey())) // singletonList: 불변
+                .securityContexts(singletonList(securityContext()))
+                .produces(singleton("application/json"))
+                .consumes(singleton("application/json"))
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(withMethodAnnotation(ApiOperation.class))
+                .build();
+    }
+
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .scopeSeparator(",")
+                .additionalQueryStringParams(null)
+                .useBasicAuthenticationWithAccessCodeGrant(false)
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Place-Server")
+                .contact(new Contact("yuna", null, "yuna0560@naver.com"))
+                .version("1.0.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("apiKey", jwtTokenConfigure.getHeader(), "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(securityReference())
+//                .operationSelector(o -> true)
+                .build();
+    }
+
+    private List<SecurityReference> securityReference() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return singletonList(new SecurityReference("apiKey", authorizationScopes));
+    }
+}
