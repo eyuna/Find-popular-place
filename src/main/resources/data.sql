d15de44b5c8cc52e8751f7f89d330045d2a22024
이유나 <yuna0560@naver.com>
2022-08-01 21:39:36 +0900

    [add] post, connections 스키마, 데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,10 @@ INSERT INTO users(name,email,profile_image_url,passwd) VALUES ('tester02','test0
 INSERT INTO stores(name,zip_code,address,lat,lng,likes) VALUES ('test store 1', '12345', 'test address0, 12345', '48.12560', '11.72610', 0);
 INSERT INTO stores(name,zip_code,address,lat,lng,likes) VALUES ('test store 2', '35355', 'test address1, 35355', '33.38339', '128.23376', 220);
 INSERT INTO stores(name,zip_code,address,lat,lng,likes) VALUES ('test store 3', '24251', 'test address2, 24251', '53.34935', '-8.76619', 3);
+
+INSERT INTO posts(store_seq,user_seq,contents,like_count,comment_count,create_at) VALUES (1, 1,'test01 first post',1,1,'2019-03-01 13:10:00');
+INSERT INTO posts(store_seq,user_seq,contents,like_count,comment_count,create_at) VALUES (2, 1,'test01 second post',0,0,'2019-03-12 09:45:00');
+INSERT INTO posts(store_seq,user_seq,contents,like_count,comment_count,create_at) VALUES (1, 1,'test01 third post',0,0,'2019-03-20 19:05:00');
+INSERT INTO posts(store_seq,user_seq,contents,like_count,comment_count,create_at) VALUES (1, 2,'test02 post',0,1,'2019-03-20 15:13:20');
+
+INSERT INTO connections(user_seq,target_seq,granted_at,create_at) VALUES (1,2,'2019-03-31 13:00:00','2019-03-31 00:10:00');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS connections CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS stores CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 -- 사용자 데이터
 CREATE TABLE users
@@ -27,4 +29,35 @@ CREATE TABLE stores
     likes               int                     DEFAULT 0,
     PRIMARY KEY (seq),
     CONSTRAINT unq_stores UNIQUE (name, zip_code)
+);
+
+CREATE TABLE posts
+(
+    seq           bigint       NOT NULL AUTO_INCREMENT,              -- POST PK
+    store_seq     bigint       NOT NULL,                             -- STORE PK
+    user_seq      bigint       NOT NULL,                             -- POST 작성자 PK
+    contents      varchar(500) NOT NULL,                             -- 내용
+    like_count    int          NOT NULL DEFAULT 0,                   -- 좋아요수
+    comment_count int          NOT NULL DEFAULT 0,                   -- 댓글수
+    create_at     datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP(), -- 생성일시
+    PRIMARY KEY (seq),
+    CONSTRAINT fk_post_to_user FOREIGN KEY (user_seq) REFERENCES users (seq) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT fk_post_to_store FOREIGN KEY (store_seq) REFERENCES stores (seq) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+
+-- 사용자들의 친구 관계 데이터이다.
+-- 해당 테이블의 Row 데이터는 `user_seq`, `target_seq`가 친구 관계임을 의미한다.
+-- 단, `user_seq` -> `target_seq` 방향으로만 친구관계가 되며, 역방향 `target_seq` -> `user_seq`은 친구가 아니다.
+-- ("`target_seq`는 `user_seq`의 친구이다"는 성립하지만, "`user_seq`는 `target_seq`의 친구이다"는 성립하지 않는다.)
+CREATE TABLE connections
+(
+    seq        bigint   NOT NULL AUTO_INCREMENT,              -- 친구관계 PK
+    user_seq   bigint   NOT NULL,                             -- 사용자 PK
+    target_seq bigint   NOT NULL,                             -- 친구 사용자 PK
+    granted_at datetime          DEFAULT NULL,                -- 친구수락일시
+    create_at  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(), -- 생성일시
+    PRIMARY KEY (seq),
+    CONSTRAINT fk_connection_to_user FOREIGN KEY (user_seq) REFERENCES users (seq) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT fk_connection_to_user2 FOREIGN KEY (target_seq) REFERENCES users (seq) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
