8c4157ef5704d170810370ffd1da3566bb77d45f
이유나 <yuna0560@naver.com>
2022-08-03 16:12:03 +0900

    [add] jackson 설정

--- src/main/java/com/hot/place/configure/ServiceConfigure.java
+++ src/main/java/com/hot/place/configure/ServiceConfigure.java
@@ -1,11 +1,22 @@
 package com.hot.place.configure;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.hot.place.security.Jwt;
 import com.hot.place.util.MessageUtils;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ServiceConfigure {
@@ -20,5 +31,22 @@ public class ServiceConfigure {
     @Bean
     public Jwt jwt(JwtTokenConfigure jwtTokenConfigure) {
         return new Jwt(jwtTokenConfigure.getIssuer(), jwtTokenConfigure.getClientSecret(), jwtTokenConfigure.getExpirySeconds());
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        // Jackson 설정 처리
+        return builder -> {
+            AfterburnerModule abm = new AfterburnerModule();
+            JavaTimeModule jtm = new JavaTimeModule();
+            jtm.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_DATE_TIME));
+
+            builder.visibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+            builder.visibility(PropertyAccessor.IS_GETTER, JsonAutoDetect.Visibility.NONE);
+            builder.visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+            builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            builder.modulesToInstall(abm, jtm);
+        };
     }
 }
