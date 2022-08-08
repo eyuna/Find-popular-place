9e678495f16a357b0893ebc0d2e431f07d6b3f0b
이유나 <yuna0560@naver.com>
2022-08-08 17:49:00 +0900

    [fix] 내정보 조회 시 api 토큰 필요

--- src/main/java/com/hot/place/controller/user/UserRestController.java
+++ src/main/java/com/hot/place/controller/user/UserRestController.java
@@ -3,12 +3,14 @@ package com.hot.place.controller.user;
 import com.hot.place.controller.ApiResult;
 import com.hot.place.error.NotFoundException;
 import com.hot.place.model.user.Email;
+import com.hot.place.security.JwtAuthentication;
 import com.hot.place.service.user.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.tomcat.jni.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,11 +32,11 @@ public class UserRestController {
 
     @GetMapping("user/me")
     @ApiOperation(value = "내 정보")
-    public ApiResult<UserDto> me(String email) {
+    public ApiResult<UserDto> me(@AuthenticationPrincipal JwtAuthentication authentication) {
         return OK(
-                userService.findByEmail(new Email(email))
+                userService.findByEmail(authentication.email)
                 .map(UserDto::new)
-                .orElseThrow(() -> new NotFoundException(User.class, email))
+                .orElseThrow(() -> new NotFoundException(User.class, authentication.email))
         );
     }
 }
